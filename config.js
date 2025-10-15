6e3fb639a5ce70b36570f6cef80b4ff1c8082608
azizt91 <21195038@mhs.stmik-tegal.ac.id>
2025-10-15 18:10:55 +0700

    chore: Update API_BASE_URL after redeployment

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================
 
 // URL Google Apps Script Web App
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbx9vxG9fzv_sFbm-sOvluJtv9xPfM4kywnCkFKKKYrOBscufsLbA_WFz2WTmfFq3ByaGA/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwGJ5TefSkQVEbLlhiFsFZAibf4TM3sFKh_AyUmOTYTVGF6j-J4-u7UPxrm91_SRj-yQA/exec';
 
 // Config object
 const API_CONFIG = {
